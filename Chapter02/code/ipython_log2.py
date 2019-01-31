48423d46f37f0af162a8d8bff2e9ebfd3a8b91bf
Kirk Dsouza <kirkdsouza@packtpub.com>
2019-01-31 11:11:18 +0530

    Codes

---
+++ Chapter02/code/ipython_log2.py
@@ -0,0 +1,3 @@
+# IPython log file
+
+get_ipython().run_line_magic('run', 'ipython_log.py')
---
+++ Chapter02/code/mergesort.py
@@ -0,0 +1,14 @@
+# thanks to https://rosettacode.org/wiki/Sorting_algorithms/Merge_sort#Python
+from heapq import merge
+ 
+def merge_sort(m):
+    if len(m) <= 1:
+        return m
+ 
+    middle = len(m) // 2
+    left = m[:middle]
+    right = m[middle:]
+ 
+    left = merge_sort(left)
+    right = merge_sort(right)
+    return list(merge(left, right))
